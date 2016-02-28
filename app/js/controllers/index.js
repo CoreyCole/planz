04afe6cdc87dc0ddf87ac93ca46f120747c16e62
Khurram Ali <khurram.ali13d@gmail.com>
2016-02-28 00:05:35 -0800

    Matt can pull now.

--- app/js/controllers/index.js
+++ app/js/controllers/index.js
@@ -4,18 +4,7 @@
  * # MainCtrl
  */
 angular.module('Planz')
-    .controller('IndexCtrl', function ($scope, $firebaseArray, $state, rootRef) {
-
-
-        $scope.register = function() {
-            console.dir($scope.plan);
-            $scope.planz = $firebaseArray(rootRef.child('Planz'));
-            $scope.plan.date = $scope.plan.date.toString();
-            $scope.plan.startTime = $scope.plan.startTime.toString();
-            $scope.planz.$add($scope.plan).then(function(ref) {
-                $state.go('start', { planid : ref.key() });
-            });
-        }
+    .controller('IndexCtrl', function ($scope,  $firebaseObject, eventfulKey, rootRef) {
         // download the data into a local object
         var syncObject = $firebaseObject(rootRef);
 
