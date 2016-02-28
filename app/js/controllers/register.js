31815f49061acbcfd6d793b2080394258d62aee1
mdnyhus <mdnyhus@gmail.com>
2016-02-28 01:17:27 -0800

    changed string type

--- app/js/controllers/register.js
+++ app/js/controllers/register.js
@@ -11,12 +11,43 @@ angular.module('Planz')
         $scope.date = new Date();
         $scope.time = moment();
         $scope.numSwipes;
+        
+        function copyText() {
+            var text = document.getElementById("shareURL");
+            text.select();
+            try {
+                var successful = document.execCommand('copy');
+                var msg = successful ? 'successful' : 'unsuccessful';
+                console.log('Copying text command was ' + msg);
+            } catch (err) {
+                console.log('Oops, unable to copy');
+            }
+        }
+        
+        function getDateEventfulFormat() {
+            var year = $scope.date.getFullYear().toString();
+            var month = $scope.date.getMonth();
+            if (month < 10) {
+                month = "0" + month.toString();
+            } else {
+                month = month.toString();
+            }
+
+            var date = $scope.date.getDate().toString();
+            if (date < 10) {
+                date = "0" + date.toString();
+            } else {
+                date = date.toString();
+            }         
+
+            return year + month + date + "00";
+        };
 
         $scope.register = function() {
             var plan = {
                 city: $scope.city,
-                date: $scope.date.toString(),
-                time: $scope.time.toString(),
+                date: getDateEventfulFormat,
+                time: $scope.time.format('LT'),
                 numSwipes: $scope.numSwipes
             };
 
