79144dce918e34ce17f9164ccb0bd3b712d49f45
Corey Cole <coreyleoc@gmail.com>
2016-02-28 00:02:34 -0800

    stuff

--- app/js/controllers/index.js
+++ app/js/controllers/index.js
@@ -4,6 +4,16 @@
  * # MainCtrl
  */
 angular.module('Planz')
-    .controller('IndexCtrl', function ($scope) {
-        
+    .controller('IndexCtrl', function ($scope, $firebaseArray, $state, rootRef) {
+
+
+        $scope.register = function() {
+            console.dir($scope.plan);
+            $scope.planz = $firebaseArray(rootRef.child('Planz'));
+            $scope.plan.date = $scope.plan.date.toString();
+            $scope.plan.startTime = $scope.plan.startTime.toString();
+            $scope.planz.$add($scope.plan).then(function(ref) {
+                $state.go('start', { planid : ref.key() });
+            });
+        }
     });
--- app/js/routes.js
+++ app/js/routes.js
@@ -6,9 +6,21 @@
 angular.module('Planz')
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
-            .state('main', {
+            .state('new', {
                 url: '/',
+                templateUrl: 'views/new.html'
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl: 'views/register.html'
+            })
+            .state('start', {
+                url: '/start/:planid',
                 templateUrl: 'views/start.html'
+            })
+            .state('swipe', {
+                url: '/swipe/:planid',
+                templateUrl: 'view/swipe.html'
             });
 
         $urlRouterProvider.otherwise('/');
