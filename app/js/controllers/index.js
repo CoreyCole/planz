7af367d511424dcb0382cfb62f836c4fbd8dbd82
Corey Cole <coreyleoc@gmail.com>
2016-02-28 00:10:18 -0800

    carArr

--- app/js/controllers/index.js
+++ app/js/controllers/index.js
@@ -32,37 +32,37 @@ angular.module('Planz')
         
         var curPage = 1;
         var maxCount = 5;
-        var catArr = [
-            { 'name':'music', 'count':0 },
-            { 'name':'conference', 'count':0 },
-            { 'name':'comedy', 'count':0 },
-            { 'name':'learning_education', 'count':0 },
-            { 'name':'family_fun_kids', 'count':0 },
-            { 'name':'festivals_parades', 'count':0 },
-            { 'name':'movies_film', 'count':0 },
-            { 'name':'food', 'count':0 },
-            { 'name':'fundraisers', 'count':0 },
-            { 'name':'art', 'count':0 },
-            { 'name':'support', 'count':0 },
-            { 'name':'holiday', 'count':0 },
-            { 'name':'books', 'count':0 },
-            { 'name':'attractions', 'count':0 },
-            { 'name':'community', 'count':0 },
-            { 'name':'business', 'count':0 },
-            { 'name':'singles_social', 'count':0 },
-            { 'name':'schools_alumni', 'count':0 },
-            { 'name':'clubs_associations', 'count':0 },
-            { 'name':'outdoors_recreation', 'count':0 },
-            { 'name':'performing_arts', 'count':0 },
-            { 'name':'animals', 'count':0 },
-            { 'name':'politics_activism', 'count':0 },
-            { 'name':'sales', 'count':0 },
-            { 'name':'science', 'count':0 },
-            { 'name':'religion_spirituality', 'count':0 },
-            { 'name':'sports', 'count':0 },
-            { 'name':'technology', 'count':0 },
-            { 'name':'other', 'count':0 }
-        ];
+        var catArr = {
+            music: {'count': 0},
+            conference: {'count': 0},
+            comedy: {'count': 0},
+            learning_education: {'count': 0},
+            family_fun_kids: {'count': 0},
+            festivals_parades: {'count': 0},
+            movies_film: {'count': 0},
+            food: {'count': 0},
+            fundraisers: {'count': 0},
+            art: {'count': 0},
+            support: {'count': 0},
+            holiday: {'count': 0},
+            books: {'count': 0},
+            attractions: {'count': 0},
+            community: {'count': 0},
+            business: {'count': 0},
+            singles_social: {'count': 0},
+            schools_alumni: {'count': 0},
+            clubs_associations: {'count': 0},
+            outdoors_recreation: {'count': 0},
+            performing_arts: {'count': 0},
+            animals: {'count': 0},
+            politics_activism: {'count': 0},
+            sales: {'count': 0},
+            science: {'count': 0},
+            religion_spirituality: {'count': 0},
+            sports: {'count': 0},
+            technology: {'count': 0},
+            other: {'count': 0}
+        };
         
         function getDateEventfulFormat() {
             var year = $scope.date.getFullYear().toString();
